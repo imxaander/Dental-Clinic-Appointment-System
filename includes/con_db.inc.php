f96dd9300fe195111965c0a1ec17a0e7d7a6f852
Xander Ison <xander22.ison@gmail.com>
2021-09-25 17:34:00 +0800

    Update con_db.inc.php

--- includes/con_db.inc.php
+++ includes/con_db.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$sname= "localhost";
-$uname= "root";
-$pword= "";
-$dbname= "appointment_system";
+$sname= "bmcyt4tdmy8gezphoedx-mysql.services.clever-cloud.com";
+$uname= "unsmamkc9ng9ew7d";
+$pword= "jROOddKy0Sm53e6hPX97";
+$dbname= "bmcyt4tdmy8gezphoedx";
 
 $con = mysqli_connect($sname, $uname, $pword, $dbname);
 
