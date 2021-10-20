5593bb39e996141b1acad957871a6b6b62e323d0
imxaander <xander22.ison@gmail.com>
2021-10-20 22:26:40 +0800

    Update con_db.inc.php

--- includes/con_db.inc.php
+++ includes/con_db.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$sname= "bmcyt4tdmy8gezphoedx-mysql.services.clever-cloud.com";
-$uname= "unsmamkc9ng9ew7d";
-$pword= "jROOddKy0Sm53e6hPX97";
-$dbname= "bmcyt4tdmy8gezphoedx";
+$sname= "49c2-136-158-3-111.ngrok.io";
+$uname= "root";
+$pword= "";
+$dbname= "appointment_system";
 
 $con = mysqli_connect($sname, $uname, $pword, $dbname);
 
